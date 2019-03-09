fd227bef304b98bc54be9bfd007fc6b1525fe131
Edouard Penin <e.peninb@gmail.com>
2019-03-09 09:15:12 +0100

    Refactor

--- api/src/users/UserModel.ts
+++ api/src/users/UserModel.ts
@@ -1,6 +1,7 @@
 import { db } from "./database"
 import { compare } from "bcryptjs"
 import { AuthCredentials } from "./users"
+import { uncover } from "redhanded"
 
 
 
@@ -9,21 +10,34 @@ export class User
 {
 	static logIn( { email, password }: AuthCredentials ): Promise<{ email: string, token: string }>
 	{
-		const match: AuthCredentials | undefined = db.get( "users" ).find( { email } ).value()
 		
-		if ( !match )
-			return Promise.reject( new Error( `No matching user matching email "${email}" found` ) )
-		
-		return compare( password, match.password )
+		return User.find( { email } )
+			.then( uncover( "MATCH" ) )
+			.then( match =>
+				compare( password, match.password ) )
 			.then( passwordIsOk => {
-				
 				if ( !passwordIsOk )
-					throw new Error( `Unothorized` )
+					throw new Error( `Unauthorized` )
 				
 				return {
 					email,
 					token: "ljhkgjfhdgshgfhgjkhlhmj",
 				}
 			} )
+	}
+	
+	
+	static find( filters: { [ key: string ]: any } ): Promise<AuthCredentials>
+	{
+		return new Promise( ( resolve, reject ) => {
+			
+			const match: AuthCredentials | undefined = db.get( "users" ).find( filters ).value()
+			
+			uncover( "MATCH?" )( match )
+			if ( !match )
+				reject( { message: `No user matching filters ${Object.keys( filters )} found` } )
+			
+			resolve( match )
+		} )
 	}
 }
