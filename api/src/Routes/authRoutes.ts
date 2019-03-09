61bc6b62c1809058474ab6d5f97013087084f586
Edouard Penin <e.peninb@gmail.com>
2019-03-09 18:38:27 +0100

    Added session in backend

--- api/src/Routes/authRoutes.ts
+++ api/src/Routes/authRoutes.ts
@@ -1,6 +1,7 @@
 import { Request, RequestHandler, Response, Router } from "express"
 import { AuthCredentials, User, UserFactory } from "../UserModel"
 import { requireFieldsGuard } from "../middlewares"
+import { uncover } from "redhanded"
 
 
 
@@ -10,8 +11,18 @@ const authenticateUserController: RequestHandler = ( req: Request, res: Response
 	const { email, password }: AuthCredentials = req.body,
 	      user: User                           = UserFactory.from( { email, password } )
 	
+	uncover( "session" )( req.session )
+	
 	try {
 		const pass = user.authenticate( password )
+		
+		if ( req.session ) {
+			req.session.user = req.session.user = {
+				email: email,
+			};
+			
+			req.session.isAuthenticated = true;
+		}
 		
 		return res
 			.status( 200 )
--- api/src/server.ts
+++ api/src/server.ts
@@ -2,9 +2,11 @@ import express from "express"
 import bodyParser from "body-parser"
 import { usersRouter } from "./Routes/usersRoutes"
 import { authRouter } from "./Routes/authRoutes"
-
+import session from "express-session"
+import env from "dotenv"
 // https://github.com/BrianDGLS/express-ts
 
+env.config()
 
 const PORT = process.env.port || 4001
 const app: express.Application = express()
@@ -12,6 +14,14 @@ const app: express.Application = express()
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( bodyParser.json() );
 
+app.use(
+	session( {
+		secret:            process.env.SESSION_SECRET!,
+		resave:            false,
+		saveUninitialized: false,
+		cookie:            { secure: false, httpOnly: true, maxAge: 3600000 },
+	} ),
+);
 
 app.get( "/", function ( req, res ) {
 	res.send( "Hello World!" );
