2ac72f9e7e207705e20b3b6462af250ffb8a6335
Edouard Penin <e.peninb@gmail.com>
2019-03-12 18:11:20 +0100

    CSRF files

--- src/api.tsx
+++ src/api.tsx
@@ -3,9 +3,12 @@ import Axios from "axios"
 
 
 
+Axios.defaults.xsrfCookieName = "csrf-token"
+Axios.defaults.xsrfHeaderName = "csrf-token"
+
+
 export const API = Axios.create( {
 	baseURL:         "/api/",
 	withCredentials: true,
 } )
-
 
