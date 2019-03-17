6e0ac2f7b1ae8e875522e0473c4a8f6aa64dd746
Edouard Penin <e.peninb@gmail.com>
2019-03-17 08:02:33 +0100

    Fresh start

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,21 @@
-import React, { Component, HTMLAttributes, useContext, useEffect } from "react";
-import "./App.css";
-import { Header } from "./Header"
-import { RouteComponentProps, Router } from "@reach/router"
-import { AuthContext, AuthProvider } from "./AuthContext"
-import { LoginPage } from "./LoginPage"
-import { HomePage } from "./HomePage"
+import * as React from "react"
+import { HTMLAttributes } from "react"
 
-
-
-
-class App extends Component
-{
-	
-	render()
-	{
-		return (
-			<AuthProvider>
-				<div className="App">
-					<Header/>
-					
-					<main className="p-4">
-						<Router>
-							<HomePage
-								path="/"
-							/>
-							<LoginPage
-								path="/login"
-							/>
-							
-							<LogoutPage
-								path="/logout"
-							/>
-							
-							<NotFoundPage default/>
-						</Router>
-					</main>
-				</div>
-			</AuthProvider>
-		);
-		
-	}
-}
-
-
-export interface LogoutPageProps extends HTMLAttributes<HTMLDivElement>, RouteComponentProps
-{
+export interface AppProps extends HTMLAttributes<HTMLDivElement>{
 
 }
 
+export function App({style= {}, className = "", children,...props}:AppProps)  {
 
-export function LogoutPage( { navigate, location, style = {}, className = "", children, ...props }: LogoutPageProps )
-{
-	// call destroy session
-	
-	// navigate to home
-	const authProvider = useContext( AuthContext )
-	
-	useEffect( () => {
-		authProvider.logout()
-			.then( () => {
-				navigate!( "/login" )
-			} )
-	} )
-	
 	return (
 		<div
 			{...props}
-			style={{ ...style }}
-			className={`${className} LogoutPage`}
+			style={{...style}}
+			className={`${className} App`}
 		>
-			Logging out... 👨‍🚀
+			App
 		</div>
 	)
 }
-
-
-export interface NotFoundPageProps extends HTMLAttributes<HTMLDivElement>, RouteComponentProps
-{
-
-}
-
-
-export function NotFoundPage( { navigate, location, style = {}, className = "", children, ...props }: NotFoundPageProps )
-{
-	
-	return (
-		<div
-			{...props}
-			style={{ ...style }}
-			className={`${className} NotFoundPage`}
-		>
-			<h1 style={{ fontSize: "120px" }}>🤷‍♂️</h1>
-			(page not found)
-		</div>
-	)
-}
-
-
-export default App;
-
 
 
