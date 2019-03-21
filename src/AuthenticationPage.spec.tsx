1c91d84bd3c563cb09c8ae8c0a4a59aa291826ae
Edouard Penin <e.peninb@gmail.com>
2019-03-21 14:34:14 +0100

    Removed comments

--- src/AuthenticationPage.spec.tsx
+++ src/AuthenticationPage.spec.tsx
@@ -40,7 +40,7 @@ feature( `A user can log in`, () => {
 	given( () => when( gatekeeper.login( CREDENTIALS ) ).thenResolve() )
 	
 	then( `I should be redirected to home`, async () => {
-		const { login, navigate, debug } = renderAuthPage()
+		const { login, navigate } = renderAuthPage()
 		
 		login( CREDENTIALS )
 		
@@ -50,7 +50,6 @@ feature( `A user can log in`, () => {
 		
 		verify( navigate( "/", undefined ), { times: 1 } )
 	} )
-	
 	
 	// @todo: Empty form cannot submit
 	// @todo: Login rejected
@@ -68,7 +67,7 @@ feature( `A user can sign up`, () => {
 	given( () => when( gatekeeper.signup( CREDENTIALS ) ).thenResolve() )
 	
 	then( `I should be redirected to home`, async () => {
-		const { signup, navigate, click, debug } = renderAuthPage()
+		const { signup, navigate, click } = renderAuthPage()
 		
 		click( /signup/i )
 		
@@ -80,7 +79,6 @@ feature( `A user can sign up`, () => {
 		
 		verify( navigate( "/", undefined ), { times: 1 } )
 	} )
-	
 	
 	// @todo: Switching tabs back and forth
 	// @todo: Extract auth method, this is the same thing as login
@@ -123,271 +121,6 @@ function renderAuthPage( gatekeep: Gatekeeper = gatekeeper )
 }
 
 
-/*
-let gatekeeper: AuthProvider
-
-beforeEach( () => {
-	(AuthenticationPageView as jest.Mock).mockClear();
-	gatekeeper = object<AuthProvider>()
-} )
-
-feature( `Only logged out users can access the page`, () => {
-	beforeEach( () => {
-		gatekeeper = object<AuthProvider>()
-	} )
-	
-	scenario( `Already logged in`, () => {
-		given( () => when( gatekeeper.isAuthenticated() ).thenReturn( true ) )
-		
-		then( `User is redirected to home`, () => {
-			const { navigate } = renderAuthPage( gatekeeper )
-			
-			verify( navigate( "/" ) )
-		} )
-	} )
-	
-	scenario( `Not logged in`, () => {
-		given( () => when( gatekeeper.isAuthenticated() ).thenReturn( false ) )
-		
-		then( `User is not redirected to home`, () => {
-			const { navigate } = renderAuthPage( gatekeeper )
-			
-			verify( navigate( "/" ), { times: 0 } )
-		} )
-	} )
-} )
-
-// @todo: those two featue (signup/login) are exactly the same, merge them
-feature( `A user can log in`, () => {
-	let credentials: authCredentials = fake<authCredentials>( "credentials" )
-	
-	scenario( `Success`, () => {
-		given( () => {
-			when( gatekeeper.isAuthenticated() ).thenReturn( false )
-			when( gatekeeper.login( credentials ) ).thenResolve()
-		} )
-		
-		then( `A loader is displayed`, async () => {
-			const { view } = renderAuthPage( gatekeeper )
-			
-			view.onLogin( credentials )
-			
-			expect( view.loading ).toBe( true )
-		} )
-		
-		and( `A success message is passed to the view`, async () => {
-			const { view } = renderAuthPage( gatekeeper )
-			
-			expect( view.authorized ).not.toBeDefined()
-			
-			view.onLogin( credentials )
-			
-			await tick()
-			
-			expect( view.authorized ).toBeDefined()
-		} )
-		
-		and( `The loader is disabled`, async () => {
-			jest.useFakeTimers()
-			
-			const { view } = renderAuthPage( gatekeeper )
-			
-			view.onLogin( credentials )
-			
-			await tick()
-			
-			expect( view.loading ).toBe( false )
-		} )
-		
-		then( `User is redirected to home after a timeout`, async () => {
-			const { view, navigate } = renderAuthPage( gatekeeper )
-			
-			view.onLogin( credentials )
-			
-			await tick()
-			
-			verify( navigate( "/" ), { times: 0 } )
-			
-			jest.advanceTimersByTime( 3000 )
-			
-			verify( navigate( "/" ) )
-		} )
-	} )
-	
-	scenario( `Login refused`, () => {
-		const error = fake<Error>( "error" )
-		
-		given( () => {
-			when( gatekeeper.isAuthenticated() ).thenReturn( false )
-			when( gatekeeper.login( credentials ) ).thenReject( error )
-		} )
-		
-		then( `An error message is passed to the view`, async () => {
-			const { view } = renderAuthPage( gatekeeper )
-			
-			view.onLogin( credentials )
-			
-			await tick()
-			
-			expect( view.error ).toBe( error )
-		} )
-		
-		and( `Loader is disabled`, async () => {
-			const { view } = renderAuthPage( gatekeeper )
-			
-			view.onLogin( credentials )
-			
-			await tick()
-			
-			expect( view.loading ).toBe( false )
-		} )
-	} )
-} )
-
-// @todo: those two featue (signup/login) are exactly the same, merge them
-feature( `A user can sign-in`, () => {
-	let credentials: authCredentials = fake<authCredentials>( "credentials" )
-	
-	scenario( `Success`, () => {
-		given( () => {
-			when( gatekeeper.isAuthenticated() ).thenReturn( false )
-			when( gatekeeper.signup( credentials ) ).thenResolve()
-		} )
-		
-		then( `A loader is displayed`, async () => {
-			const { view } = renderAuthPage( gatekeeper )
-			
-			view.onSignup( credentials )
-			
-			expect( view.loading ).toBe( true )
-		} )
-		
-		and( `A success message is passed to the view`, async () => {
-			const { view } = renderAuthPage( gatekeeper )
-			
-			expect( view.authorized ).not.toBeDefined()
-			
-			view.onSignup( credentials )
-			
-			await tick()
-			
-			expect( view.authorized ).toBeDefined()
-		} )
-		
-		and( `The loader is disabled`, async () => {
-			jest.useFakeTimers()
-			
-			const { view } = renderAuthPage( gatekeeper )
-			
-			view.onSignup( credentials )
-			
-			await tick()
-			
-			expect( view.loading ).toBe( false )
-		} )
-		
-		then( `User is redirected to home after a timeout`, async () => {
-			const { view, navigate } = renderAuthPage( gatekeeper )
-			
-			view.onSignup( credentials )
-			
-			await tick()
-			
-			verify( navigate( "/" ), { times: 0 } )
-			
-			jest.advanceTimersByTime( 3000 )
-			
-			verify( navigate( "/" ) )
-		} )
-	} )
-	
-	scenario( `Sign-up error`, () => {
-		const error = fake<Error>( "error" )
-		
-		given( () => {
-			when( gatekeeper.isAuthenticated() ).thenReturn( false )
-			when( gatekeeper.signup( credentials ) ).thenReject( error )
-		} )
-		
-		then( `An error message is passed to the view`, async () => {
-			const { view } = renderAuthPage( gatekeeper )
-			
-			view.onSignup( credentials )
-			
-			await tick()
-			
-			expect( view.error ).toBe( error )
-		} )
-		
-		and( `Loader is disabled`, async () => {
-			const { view } = renderAuthPage( gatekeeper )
-			
-			view.onSignup( credentials )
-			
-			await tick()
-			
-			expect( view.loading ).toBe( false )
-		} )
-	} )
-} )
-
-feature( `Switching between login and sign-up tab`, () => {
-	test( `Login tab is active by default`, () => {
-		const { view } = renderAuthPage( gatekeeper )
-		
-		expect( view.tab ).toBe( "login" )
-	} )
-	
-	test( `View gets the new activated tab`, () => {
-		const { view } = renderAuthPage( gatekeeper )
-		
-		view.onTabSelect( "signup" )
-		expect( view.tab ).toBe( "signup" )
-		
-		view.onTabSelect( "login" )
-		expect( view.tab ).toBe( "login" )
-		
-		view.onTabSelect( "login" )
-		expect( view.tab ).toBe( "login" )
-	} )
-} )
-
-xtest( `Separating "state" (loading, tab) and "dispatchers" (onlogin, onsigup,...)`, () => {
-	fail()
-} )
-
-xtest( `Does the design tell the right story, doesn't this do too much ?`, () => {
-	// maybe the only goal of this thing is to redirect
-	// and then there's a loginOrSignup component that handles the tab part
-	// and the a login and a signup component thath handle the submit
-	// the controller doesn't care which tab is active, it cares about signing up or logging in and preventing page access -> or rather providing the required data. nothing else
-	// i like a useGuard or a protected route component, the separation of singup/login logic i don't know
-	fail()
-} )
-
-function renderAuthPage( gatekeeper: AuthProvider )
-{
-	const navigate = func<NavigateFn>(),
-	      wrapper  = render( <AuthenticationPage
-		      gatekeeper={gatekeeper}
-		      navigate={navigate}
-	      /> )
-	
-	const view: AuthenticationPageViewProps = new Proxy( {}, {
-		get( target, prop )
-		{
-			return [ ...(AuthenticationPageView as jest.Mock).mock.calls ].last()[ 0 ][ prop ]
-		},
-	} ) as any
-	
-	return {
-		...wrapper,
-		navigate,
-		gatekeeper,
-		view,
-	}
-}
-*/
 
 
 function fake<T>( name: string ): T
