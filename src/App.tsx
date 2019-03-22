ea314c5595dec021439487f58764479353bacc02
Edouard Penin <e.peninb@gmail.com>
2019-03-22 15:35:45 +0100

    Blank slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from "react"
 import { HTMLAttributes } from "react"
 import { Router } from "@reach/router"
-import { Gatekeeper } from "./Gatekeeper"
-import { AuthPage } from "./AuthenticationPage"
 import { HomePage } from "./HomePage"
 
 
@@ -10,11 +8,10 @@ import { HomePage } from "./HomePage"
 
 export interface AppProps extends HTMLAttributes<HTMLDivElement>
 {
-	gatekeeper: Gatekeeper
 }
 
 
-export function App( { gatekeeper, style = {}, className = "", children, ...props }: AppProps )
+export function App( { style = {}, className = "", children, ...props }: AppProps )
 {
 	
 	return (
@@ -25,11 +22,6 @@ export function App( { gatekeeper, style = {}, className = "", children, ...prop
 		>
 			<Router>
 				<HomePage path="/"/>
-				
-				<AuthPage
-					path="/authenticate"
-					gatekeeper={gatekeeper}
-				/>
 			</Router>
 		</div>
 	)
--- src/index.tsx
+++ src/index.tsx
@@ -3,27 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { authCredentials, Gatekeeper } from "./Gatekeeper"
 
 
 
 
-const gatekeeper: Gatekeeper = {
-	signup( credentials: authCredentials ): Promise<void>
-	{
-		return Promise.resolve()
-	},
-	login( credentials: authCredentials ): Promise<void>
-	{
-		return Promise.resolve()
-	},
-	authenticated(): boolean
-	{
-		return false
-	},
-}
-
-ReactDOM.render( <App gatekeeper={gatekeeper}/>, document.getElementById( "root" ) );
+ReactDOM.render( <App/>, document.getElementById( "root" ) );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
