fb0732e0a830040a0039e25c2c746b437e765309
Edouard Penin <e.peninb@gmail.com>
2019-03-24 17:59:47 +0100

    User is redirected to home on login success

--- src/AuthPage.spec.tsx
+++ src/AuthPage.spec.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
-import { HTMLAttributes, useContext, useEffect } from "react"
+import { FormEvent, HTMLAttributes, useContext, useEffect } from "react"
 
 import { Case, Feature, Given, Then } from "jest-then"
-import { appRender } from "./testUtils"
+import { appRender, tick } from "./testUtils"
 import { RouteComponentProps } from "@reach/router"
 import { object, verify, when } from "testdouble"
 import { ServicesContext } from "./ServicesContext"
@@ -10,9 +10,18 @@ import { ServicesContext } from "./ServicesContext"
 
 
 
+export interface credentials
+{
+	email: string
+	password: string
+}
+
+
 export interface Gatekeeper
 {
 	isAuthenticated(): boolean;
+	
+	login( credentials: credentials ): Promise<void>
 }
 
 
@@ -30,13 +39,45 @@ export function AuthenticationPage( { navigate, style = {}, className = "", chil
 			navigate!( "/" )
 	} )
 	
+	
+	function handleSubmit( e: FormEvent<HTMLFormElement> )
+	{
+		e.preventDefault()
+		const data     = new FormData( e.target as HTMLFormElement ),
+		      email    = data.get( "email" ) as string | undefined,
+		      password = data.get( "password" ) as string | undefined
+		
+		if ( !email || !password )
+			return
+		
+		gatekeeper.login( { email, password } )
+			.then( () => navigate!( "/" ) )
+	}
+	
+	
 	return (
 		<div
 			{...props}
 			style={{ ...style }}
 			className={`${className} AuthenticationPage`}
 		>
-			AuthenticationPage
+			<form onSubmit={handleSubmit}>
+				<label>
+					Email
+					<input type="email"
+					       name="email"
+					       placeholder="Email"/>
+				</label>
+				
+				<label>
+					Password
+					<input type="password"
+					       name="password"
+					       placeholder="Password"/>
+				</label>
+				
+				<button type="submit">Log me in</button>
+			</form>
 		</div>
 	)
 }
@@ -67,18 +108,31 @@ Feature( `User is redirected to home if already logged in`, () => {
 } )
 
 Feature( `User can log in`, () => {
-	const gatekeeper = object<Gatekeeper>()
+	const gatekeeper               = object<Gatekeeper>(),
+	      credentials: credentials = { email: "user@email.com", password: "$password$" }
 	
 	Given( () => when( gatekeeper.isAuthenticated() ).thenReturn( false ) )
 	
-	
 	Case( "Authorized", () => {
-		// Given( () => when( gatekeeper.isAuthenticated() ).thenReturn( true ) )
 		
-		Then( "User is redirected to home", () => {
-			// const { navigate } = appRender( "/auth", { gatekeeper } )
-			//
-			// verify( navigate( "/", undefined ) )
+		Given( () => when( gatekeeper.login( credentials ) ).thenResolve() )
+		
+		when( () => {
+		
+		} )
+		
+		Then( "User is redirected to home", async () => {
+			const { navigate, fill, click, submit } = appRender( "/auth", { gatekeeper } )
+			
+			fill( /email/i, credentials.email )
+			
+			fill( /password/i, credentials.password )
+			
+			click( /log me in/i )
+			
+			await tick()
+			
+			verify( navigate( "/", undefined ) )
 		} )
 	} )
 	
